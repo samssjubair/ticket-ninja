044d32e572373f3c14bc91ff9123c602938203b0
samssjubair <samssjubair@gmail.com>
2021-01-30 02:59:06 +0600

    book now button handled

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 function ticketAmountHandler(isIncrease, category) {
-  let fClassAmount = parseInt(document.getElementById(category+"Input").value);
+  let fClassAmount = parseInt(
+    document.getElementById(category + "Input").value
+  );
 
-  if (isIncrease ) {
+  if (isIncrease) {
     fClassAmount = fClassAmount + 1;
   } else {
     fClassAmount = fClassAmount - 1;
   }
   if (fClassAmount >= 0) {
-    document.getElementById(category+"Input").value = fClassAmount;
+    document.getElementById(category + "Input").value = fClassAmount;
     amountCalculation();
   }
 }
-
-function amountCalculation() {
+function subTotalPrice() {
   const fClassAmount = parseInt(document.getElementById("fClassInput").value);
   const economyAmount = parseInt(document.getElementById("economyInput").value);
   const subTotal = fClassAmount * 150 + economyAmount * 100;
+
+  return subTotal;
+}
+
+function amountCalculation() {
+    const subTotal=subTotalPrice();
   const tax = subTotal * 0.1;
   const total = subTotal + tax;
   document.getElementById("subTotal").innerText = subTotal;
   document.getElementById("vat").innerText = tax;
   document.getElementById("total").innerText = total;
+}
+
+function bookNowHandler() {
+    const subTotal=subTotalPrice()+subTotalPrice()*.1;
+  document.getElementById("success-total").innerText = subTotal ;
+  document.getElementById("success-section").style.display = "block";
+  document.getElementById("main-section").style.display = "none";
 }
